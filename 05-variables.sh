c19979d153cfcfd5582447288e68241af4160378
sivakumar reddy <info@joindevops.com>
2025-09-19 07:35:48 +0530

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Please enter your PIN number"
-read PIN_NUMBER # here PIN_NUMBER is variable
+read -s PIN_NUMBER # here PIN_NUMBER is variable
 
 echo "PIN entered is: $PIN_NUMBER"
