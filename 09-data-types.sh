665e03313e24ecfe6ee02a30b9ca3f76b633a282
sivakumar reddy <info@joindevops.com>
2025-09-19 08:33:19 +0530

    variables

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -9,7 +9,9 @@ SUM=$(($NUMBER1+$NUMBER2+$NAME))
 
 echo "SUM is: ${SUM}"
 
+# Size = 4, Max index = 3
 LEADERS=("Modi" "Putin" "Trudo" "Trump")
 
 echo "All leaders: ${LEADERS[@]}"
 echo "First Leader: ${LEADERS[0]}"
+echo "First Leader: ${LEADERS[10]}"
